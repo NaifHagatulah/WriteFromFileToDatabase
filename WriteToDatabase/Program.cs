372acb6871167c399e9cdae27ca88d7f7f22cc02
NaifHagatulah <naifhagatulah@hotmail.com>
2022-12-17 16:54:01 +0100

    write to database

---
+++ WriteToDatabase/Program.cs
@@ -0,0 +1,14 @@
+﻿using Npgsql;
+
+public class WriteToDatabase
+{
+    public static void Main() 
+    {
+        
+    }
+
+    public static void WriteToDatabase(string connectionString, string query) 
+    {
+        using (NpgsqlConnection )
+    }
+}
